51235af98280b29d241e0512fa2d02e53ba020f1
Lucian Haj <lucianhaj264@gmail.com>
2021-02-22 21:23:56 -0800

    Completed 4 threads, commenting and formatting needed

--- assignment4.c
+++ assignment4.c
@@ -13,6 +13,7 @@
 #include <signal.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <assert.h>
 
 
 
@@ -24,15 +25,18 @@
 pthread_mutex_t mue1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mue2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mue3 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mue4 = PTHREAD_MUTEX_INITIALIZER;
+
 pthread_cond_t full1 =  PTHREAD_COND_INITIALIZER;
 pthread_cond_t full2 =  PTHREAD_COND_INITIALIZER;
 pthread_cond_t full3 =  PTHREAD_COND_INITIALIZER;
+pthread_cond_t full4 =  PTHREAD_COND_INITIALIZER;
 
 
 int b1_count = 0;
 int b2_count = 0;
 int b3_count = 0;
-
+int b4_count = 0;
 
 
 
@@ -40,19 +44,38 @@ int b3_count = 0;
 
 //Here the functions we are creating the different buffers which will 
 // contain 50 character arrays of length 1000 
-char ** create_buff(){
-	char ** buff[50];
 
-for(int i = 0; i < 50; i++){
-	buff[i] = (char*) malloc(1000 * sizeof(char));
+char  buff1[1024]= {0};
+char  buff2[1024] = {0};
+
+char buff3[1024] = {0};
+
+// requires len(replacement_term) <= len(term)
+char* replaceTerm(const char *line, const char *term, const char *replacement_term)
+{
+	// fail if we have any null args
+	assert(line);
+	assert(term);
+	assert(replacement_term);
+	
+    // note this depends on the target line len <= source line length
+    char *buf = calloc(strlen(line)+1, sizeof(char));
+    const char *p = NULL;
+    const char *rest = line;
+    // while we are still finding term 
+    while((p = strstr(rest, term)))
+    {
+        strncat(buf, rest, (size_t)(p - rest));
+        strcat(buf, replacement_term);
+        rest = p + strlen(term);
+    }
+   // no more term, so just tack on what's left
+    strcat(buf, rest);
+    return buf;
 }
-	return buff;
-}
 
-char ** buff1[50];
-char ** buff2[50];
-char ** buff3[50];
 
+//char ** buff3[50];
 
 
 
@@ -77,212 +100,124 @@ char * read_line(FILE * stream){
   */
 
 void *read_line(void * stream){
-	// while !STOP loop here
-		
-		//{
-	char * tmp_str = (char*)malloc(1000 * sizeof(char));
 
-	int count = 0;
-	//FILE * stream = *((FILE
+	char * tmp_str = (char*) calloc(1024, sizeof(char));
+	int stop = 0;
+
+
 	
-	
-	
-	//while(strcmp(tmp_str, "STOP") != 0){
-	 fgets(tmp_str, 80, (FILE *)stream);
-		printf("what is the line %s \n", tmp_str);
+	while(!stop){
+	 fgets(tmp_str, 1000, stdin);
 
 	pthread_mutex_lock(&mue1);
-	 buff1[b1_count] =(char*)malloc(1000 * sizeof(char));
-	 strcpy(buff1[b1_count], tmp_str); 
+	 strcpy(buff1, tmp_str); 
 	b1_count++;
 	
 	pthread_cond_signal(&full1);
 	pthread_mutex_unlock(&mue1);
-	//printf("what is the line %s \n", buff1[b1_count-1]);
-		// } 
-	//free(tmp_str); 
-	return NULL;
-		//}
 	
-} 
-void * seperate_line(void * parameter){
-	int count = 0;
-			/* pthread_mutex_lock(&mue1);
-		while(b1_count == 0){
-		pthread_cond_wait(&full1, &mue1);
-		
-		}	
-	if(strcmp(buff1[b1_count], "STOP") == 0){
 	
-			return NULL;
+	
+	// wait until the print thread has dealt with this line
+		pthread_mutex_lock(&mue4);
+		while (b4_count == 0) {
+			pthread_cond_wait(&full4, &mue4);
 		}
-		pthread_mutex_unlock(&mue1); */
-			
-	 
-	pthread_mutex_lock(&mue1);
-		while(b1_count == 0){
-		pthread_cond_wait(&full1, &mue1);
-		}
-		 
-		pthread_mutex_lock(&mue2); 
-		 buff2[b2_count] = (char *)malloc(1000 * sizeof(char));
-		strcpy(buff2[b2_count], buff1[b1_count-1]); 
-		buff2[b2_count] = buff1[b1_count-1];
+		b4_count++;
+		pthread_mutex_unlock(&mue4);
 
+	
+	if(strncmp(tmp_str, "STOP\n", 5) == 0){
+		stop = 1;
+	}
+	
+	} 
+		 
+	return NULL;
+	
+ 
+}
+
+void* line_sep(void* args) {
+	int stop = 0;
+	char *line = calloc(1000, sizeof(char));
+	
+	//Tokenizes input based on "\n" and places it into buffer 2 to be seperated by an empty space
+	while (! stop) {
+		
+		// wait for lock then copy data locally
+		pthread_mutex_lock(&mue1);
+		while (b1_count == 0) {
+			pthread_cond_wait(&full1, &mue1);
+		}
+		
+		strcpy(line, buff1);
+		b1_count--;
+		pthread_mutex_unlock(&mue1);
+
+		// check for STOP before we remove the newline
+		if (strncmp(line, "STOP\n", 5) == 0) {
+			stop = 1;
+		}
+		else{
+		int len = strlen(line);
+			if (line[len - 1] == '\n') {
+				line[len - 1] = ' ';
+			}
+			
+		}
+		// process the current line replace \n with space
+
+		// lock buffer and copy data for next thread
+		pthread_mutex_lock(&mue2);
+		strcpy(buff2, line);
 		b2_count++;
 		pthread_cond_signal(&full2);
-
 		pthread_mutex_unlock(&mue2);
-	while(count < 1000){
-		
-		  if(buff1[b1_count-1][count] == '\n'){
-		
-			buff1[b1_count-1][count] = ' ';
-		}
-		  
-		
+	}
 
-				count++;
-
-		}
-		b1_count--;
-
-		pthread_mutex_unlock(&mue1);
-		 
-
-	
-		 
-		 
-		
-		
-		printf("buff2 at %d contains %s:", b2_count-1, buff2[b2_count-1]); 
-		return NULL;
-		
-		
-		
-		
-		
-		
-		//printf("buff1 contains: %s \n", buff1[b1_count+1]);
-	
-	
-	
+	return NULL;
 }
 
-// char * seperate_line(int * stop){
-	// int count = 0;
 	
-	// while(count < 1000){
-		// if(buff1[b1_count][count] == '\n'){
-			// buff1[b1_count][count] = ' ';
-		// }
+void * plus_replace(void * s){
+	char * new_line;
+	int stop = 0;
+	
+	char *line = calloc(1000, sizeof(char));
 
-		// if(strcmp(buff1[b1_count], "STOP") == 0){
-			// *stop = 1;
-			// break; 
-		// }
-				// count++;
+	
+	while(!stop){
+    pthread_mutex_lock(&mue2);
+	while(b2_count == 0){
+		pthread_cond_wait(&full2, &mue2);
+		}
+	strcpy(line, buff2);
+	b2_count--;
+ 
+	pthread_mutex_unlock(&mue2); 
+	
+	if(strncmp(line, "STOP\n", 5) == 0){
+		stop = 1;
+	new_line = line;
+	}
+	else{
+	new_line = replaceTerm(line, "++", "^");
 
-		// }
-	// buff2[b2_count] = (char *)malloc(1000 * sizeof(char));
-		// strcpy(buff2[b2_count], buff1[b1_count]);
-		
-		// printf("buff1 contains: %s \n", buff2[b1_count]);
-	
-	
-	
-// }
-void place_in_buffer(char ** buffer){
-	
-	
-	
-	
-	
-	
-	
-	
-}
-		
-	
-	
-void * plus_replace(char ** buff2){
-	 /* char ** line; 
-	line = (char**)parameter;  */
-	int star_count = 1;
-	int count = 2;
-	int i = 0; 
-	 printf("what is replace %s \n:", buff2[b2_count-1]);
- 	for(i = 0; i < 80; i++){
-		printf("iter \n");
-		 if(buff2[b2_count-1][i+1] == NULL){
-			
-			break;
-		} 
-
-	if(buff2[b2_count-1][i] == 43 && buff2[b2_count-1][i+1] == 43 && buff2[b2_count-1][i-1] == 32){
-		//++ and then ++ again
 		
 		
-		//add in a bunch of if(buff2[i+count] != NULL)
-		if(buff2[b2_count-1][i+count] != NULL){
-		while(buff2[b2_count-1][i+count] == 43){
-		count+= 2; 
-		star_count += 1;
-		
-	
-		}
-		}
-		//remove spaces
-		for(int x = 0; x < star_count; x++){
-			if(buff2[b2_count-1][i + x] != NULL){
-			buff2[b2_count-1][i + x] = 17;
-			}
-			
-		}
-		for(int z = 0; z < star_count; z++){
-			if(buff2[b2_count-1][i+count] != NULL){
-			buff2[b2_count-1][i + star_count + z] = 94;
-			}
-			
-		}
-		i += count; 
-		count = 2;
-		star_count = 1;
-		//buff2[i+2] = 94;
-	}
-	else if(buff2[b2_count-1][i] == 43 && buff2[b2_count-1][i+1] == 43 && buff2[b2_count-1][i-1] != 32){
-		if(buff2[b2_count-1][i+count] != NULL){
-		while(buff2[b2_count-1][i+count] == 43){
-		count+= 2; 
-		star_count += 1;
-	
-		}
-		}
-		for(int x = 0; x < star_count; x++){
-			if(buff2[b2_count-1][i + x] != NULL){
-			buff2[b2_count-1][i + x] = 94;
-			}
-			
-		}
-		for(int z = 0; z < star_count; z++){
-			if(buff2[b2_count-1][i + star_count + z] != NULL){
-			buff2[b2_count-1][i + star_count + z] = 17;
-			}
-		}
-		i += count; 
-		count = 2;
-		star_count = 1;
-	}
-	else if(buff2[b2_count-1][i] == 43 && buff2[b2_count-1][i+1] == 43 && buff2[b2_count-1][i+2] != 32){
-		
-		
-		buff2[b2_count-1][i] = 94;
-		buff2[b2_count-1][i+1] = 32;
-	}
 	}
 	
 	
-		printf("what is the buff2: %s \n", buff2[b2_count-1]);
+	
+	pthread_mutex_lock(&mue3);
+	  
+	strcpy(buff3, new_line);
+	b3_count++;
+	pthread_cond_signal(&full3);
+	pthread_mutex_unlock(&mue3);   
+		
+	}
 	return NULL; 
 	
 	
@@ -292,30 +227,54 @@ void * plus_replace(char ** buff2){
 
 
 
-char * output_line(char * out){
+void * output_line(void * out){
+	char * print_buffer = (char*) malloc(50000 * sizeof(char));
+	char *line = calloc(1000, sizeof(char));
 
-	int i = 0;
-	int count = 0;
-	b3_count = strlen(buff1[b1_count]);
-	
-		
-
-/* 	if(out[i] == 17){
-	} */
-	fputs(buff1[b1_count], stdout);
+	int stop = 0;
+	int last_line = 0;
+	char output[81] = {0};
+	//fputs(buff1[b3_count-1], stdout);
+	while(!stop){	
+	pthread_mutex_lock(&mue3);	
+	while(b3_count == 0){
+		pthread_cond_wait(&full3, &mue3);
+	}
+	strcpy(line, buff3);
+	strcat(print_buffer, buff3);
 	b3_count--;
+	pthread_mutex_unlock(&mue3);
 	
-	count = i;
-	fputc('\n', stdout);
+	// now that content is in our local storage, print it
+		int cur_line_count = (strlen(print_buffer))/80;
+		for (int line = last_line; line < cur_line_count; line++) {
+			memset(output, 0, 81); // clear the output line
+			strncpy(output, print_buffer + (line*80), 80);
+				printf("%s\n", output);
+			fflush(stdout);
+		}
+		last_line = cur_line_count;
+
 	
+	
+	if(strncmp(line, "STOP\n", 5) == 0){
+		stop = 1;
+	}
+	// let the line input thread know we are done with printing
+		pthread_mutex_lock(&mue4);
+		b4_count--;
+		pthread_cond_signal(&full4);
+		pthread_mutex_unlock(&mue4);
+
+	}
 	
 
-	return out;
 
-
+	return NULL; 
+ 
 }
 
-
+ 
 
 
 
@@ -327,34 +286,32 @@ FILE * stream;
 int stop = 0;
 int target = 0; 
 
-	stream = fopen("./input1-1.txt", "r");
-
-char ** buf1;
-char ** buf2;
-char ** buf3;
-/* char * buf1 = malloc(sizeof(char)*1000);
- char * buf2 = malloc(sizeof(char)*1000);
-char * buf3 = malloc(sizeof(char)*1000);  */
-
-char * output = malloc(sizeof(char) * 1000);
-	for(int i = 0; i < 1000; i++){
-		output[i] = 0;
-	} 
+ 
 	pthread_t rd_line;
 	pthread_t sp_line; 
 	pthread_t p_r_l;
-	pthread_t output_line;
+	pthread_t out_l;
 	
 	
-	pthread_create(&rd_line, NULL, read_line, (void *) stream);
-	pthread_create(&sp_line, NULL, seperate_line, NULL);
-	pthread_create(&p_r_l, NULL, plus_replace, buff2);
+	pthread_create(&rd_line, NULL, read_line, NULL);
+	pthread_create(&sp_line, NULL, line_sep, NULL);
+	pthread_create(&p_r_l, NULL, plus_replace, NULL);
+	pthread_create(&out_l, NULL, output_line, NULL);
 	pthread_join(rd_line, NULL);
 	pthread_join(sp_line, NULL);
 	
 	pthread_join(p_r_l, NULL);
+		pthread_join(out_l, NULL);
 
-/* 
+
+return 0;
+}
+
+
+
+	//stream = fopen("./input1-1.txt", "r");
+	
+	/* 
 if(argv[1] == "<" && argv[3] == NULL){
 	result = fopen(argv[2], O_RDONLY);
 	dup2(result, 0);
@@ -389,4 +346,3 @@ output = output_line(buff1);
  
 
 //}
-}
